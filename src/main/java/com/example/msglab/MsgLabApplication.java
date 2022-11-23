7dd61f01369bad04397c23f995853f9885e776c1
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-23 13:57:36 +0900

    init

---
+++ src/main/java/com/example/msglab/MsgLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.msglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msglab/MsgLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
