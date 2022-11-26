c379f7ce24ca18e5868a7a3bbe7a3ac7da552ce0
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-27 00:45:51 +0900

    refactor : 테스트에 이름을 명시

--- src/test/java/com/example/msglab/application/MessageServiceTest.java
+++ src/test/java/com/example/msglab/application/MessageServiceTest.java
@@ -3,11 +3,13 @@ package com.example.msglab.application;
 import com.example.msglab.MessageDummy;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MessageServiceTest {
 
     @Test
+    @DisplayName("메세지 서비스의 send 메소드가 올바르게 동작하는지 테스트")
     void test1() throws JsonProcessingException {
         MessageService messageService = new MessageService();
         String id = messageService.send(MessageDummy.message);
