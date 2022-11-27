adb922bd1ba3394dc48cd047419c503271a43f0b
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-27 09:01:28 +0900

    refactor : json creator 대신 getter 이용하게 변경

--- src/main/java/com/example/msglab/domain/Message.java
+++ src/main/java/com/example/msglab/domain/Message.java
@@ -1,13 +1,13 @@
 package com.example.msglab.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 public class Message {
     private final String to;
     private final Notification notification;
 
-    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public Message(@JsonProperty("to") String to, @JsonProperty("notification") Notification notification) {
         this.to = to;
         this.notification = notification;
--- src/main/java/com/example/msglab/domain/Notification.java
+++ src/main/java/com/example/msglab/domain/Notification.java
@@ -1,13 +1,13 @@
 package com.example.msglab.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 public class Notification {
     private final String title;
     private final String body;
 
-    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public Notification(@JsonProperty("title") String title, @JsonProperty("body") String body) {
         this.title = title;
         this.body = body;
