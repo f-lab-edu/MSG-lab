46719ad5149525c07ea0ead4c41575f870d2386a
Bue-von-hon <dkssudvn2@gmail.com>
2022-11-29 09:35:08 +0900

    refactor: 불필요한 테스트 삭제

--- src/test/java/com/example/msglab/application/MessageServiceTest.java
+++ src/test/java/com/example/msglab/application/MessageServiceTest.java
@@ -12,7 +12,6 @@ class MessageServiceTest {
     @DisplayName("메세지 서비스의 send 메소드가 올바르게 동작하는지 테스트")
     void test1() throws JsonProcessingException {
         MessageService messageService = new MessageService();
-        String id = messageService.send(MessageDummy.message);
-        Assertions.assertEquals("-1", id);
+        // todo(hun): 이후 작업에서 서비스의 내용이 채워지면, 테스트 추가하기
     }
 }
