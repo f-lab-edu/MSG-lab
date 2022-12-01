502d19d1e05bf9e5e0c8c5c6b59a425a4e89160b
Bue-von-hon <dkssudvn2@gmail.com>
2022-12-01 10:16:50 +0900

    refactor : handler에서 구체적인 익셉션 캐치하게 수정
    발생 가능한 3가지 익셉션을 잡아 처리하게 수정

--- src/main/java/com/example/msglab/adapter/MessageController.java
+++ src/main/java/com/example/msglab/adapter/MessageController.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +20,7 @@ public class MessageController {
 
     private final MessageService service;
 
-    @ExceptionHandler
+    @ExceptionHandler({HttpMessageConversionException.class, HttpMessageNotReadableException.class, HttpMessageNotWritableException.class})
     public ResponseEntity<String> handle(Exception exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
