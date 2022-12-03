72a0df09660d44ab9818a58dc1188176ed56ed7a
Bue-von-hon <dkssudvn2@gmail.com>
2022-12-03 12:19:52 +0900

    testFixtures에 jackson 의존성 추가
    배경
    #17 이슈를 해결했습니다.
    
    작업 내용
    추가 : build.gradle에서 testFixtures 모듈에 jackson 의존성 추가
    수정 : JsonRequestDummy에서 객체를 json으로 변환해서 저장, 하드코딩 X
    삭제 : settings.gradle에 공란 삭제

--- src/testFixtures/java/com/example/msglab/JsonRequestDummy.java
+++ src/testFixtures/java/com/example/msglab/JsonRequestDummy.java
@@ -1,8 +1,24 @@
 package com.example.msglab;
 
+import com.example.msglab.domain.Message;
+import com.example.msglab.domain.Notification;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonRequestDummy {
 
-    public static String correctValue = "{\"to\":\"/topics/news\",\"notification\":{\"title\":\"Breaking News\",\"body\":\"asdad\"}}";
+    public static final String correctValue;
 
-    public static String NotCorrectValue = "{\"to\": \"/topics/news\",\"notification\": {\"title\": \"\",\"body\": \"asdad\"}}";
+    public static final String NotCorrectValue;
+
+    static {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            correctValue = objectMapper.writeValueAsString(new Message("/topics/news", new Notification("Breaking News", "asdad")));
+            Message invalidMessage = new Message("/topics/news", new Notification("", "asdad"));
+            NotCorrectValue = objectMapper.writeValueAsString(invalidMessage);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
