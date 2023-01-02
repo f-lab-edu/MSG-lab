cfb0293d6cbb772057b121e6f54ba8eb42bf2e1e
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-02 17:09:11 +0900

    rabbitMQ 워커 커밋만 체리픽

--- RabbitMQ-worker/src/main/java/com/example/rabbitmqworker/adapter/outbound/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rabbitmqworker.adapter.outbound;
-
-import com.example.rabbitmqworker.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, String> {
-}
