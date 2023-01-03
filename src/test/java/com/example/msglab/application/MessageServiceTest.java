686071bdb804ee37643e5ad3f89a267558b05504
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-03 13:39:57 +0900

    Fix: RabbitTemplate 대신 MessageBrokerClient 인터페이스를 사용하게 수정

--- src/test/java/com/example/msglab/application/MessageServiceTest.java
+++ src/test/java/com/example/msglab/application/MessageServiceTest.java
@@ -5,10 +5,10 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.example.msglab.MessageDummy;
+import com.example.msglab.domain.MessageBrokerClient;
 import com.example.msglab.domain.MessageClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 class MessageServiceTest {
 
@@ -16,8 +16,8 @@ class MessageServiceTest {
     @DisplayName("MessageService의 send 메소드가 MessageClient의 send 메소드를 호출하는지 테스트")
     void test1() {
         MessageClient messageClient = mock(MessageClient.class);
-        RabbitTemplate rabbitTemplate = mock(RabbitTemplate.class);
-        PostMessageService messageService = new PostMessageService(messageClient, rabbitTemplate);
+        MessageBrokerClient messageBrokerClient = mock(MessageBrokerClient.class);
+        PostMessageService messageService = new PostMessageService(messageClient, messageBrokerClient);
         messageService.send(MessageDummy.message);
         verify(messageClient, times(1)).send(MessageDummy.message);
     }
