a90c72625a912fb933da004255b241eed52b7323
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-13 23:42:02 +0900

    배경 코드리뷰를 바탕으로 익셉션에 대해 리팩토링 여러 익셉션이 아니라 특정 익셉션에 대해 handle 하도록 수정 올바른 에러 메세지를 위해 응답 클래스 추가
    작업 내용
    삭제: ApiExceptionHandler.java
    삭제: JsonConvertException.java
    추가: JsonConvertProblemDetail.java 응답 클래스 추가
    추가: PushMessageApiExceptionAdvice.java 푸시 메세지 도메인의 익셉션을 처리하는 클래스 추가
    추가: PushMessageJsonResponseDummy.java 익셉션에 대한 http 응답 검증용 dummy
    수정: PushMessageJsonRequestDummy.java "{" 문자가 빠진 json 요청 dummy 추가
    추가: JsonConvertProblemDetailTest.java getDetail 메소드 테스트
    수정: MessageControllerTest.java Dummy의 변수 이름이 변경되어 수정
    추가: PushMessageApiExceptionAdviceTest.java 잘못된 요청에 대해 준비된 응답을 내려주는지 테스트 케이스 추가
    
    결과
    앞으로 JsonCnversion 익셉션에 대해서는 RFC7807에 정의된 응답을 흉내내어 내려줍니다.
    클라이언트는 원인이 무엇인지, 어떻게 익셉션을 해결할 수 있는지 알 수 있습니다.

--- MSG-lab-api/src/test/java/com/example/msglabapi/adapter/inbound/MessageControllerTest.java
+++ MSG-lab-api/src/test/java/com/example/msglabapi/adapter/inbound/MessageControllerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.example.msglab.JsonRequestDummy;
+import com.example.msglab.PushMessageJsonRequestDummy;
 import com.example.msglabapi.application.PostMessageService;
 
 class MessageControllerTest {
@@ -21,7 +21,7 @@ class MessageControllerTest {
     @DisplayName("POST /push-message가 요청되면 MessageController에서 올바른 응답을 내려주는지 테스트")
     void test1() throws Exception {
         mockMvc.perform(post("/push-message").contentType("application/json")
-                                             .content(JsonRequestDummy.correctValue))
+                                             .content(PushMessageJsonRequestDummy.CORRECT_VALUE))
                .andExpect(status().isOk());
     }
 
@@ -29,7 +29,7 @@ class MessageControllerTest {
     @DisplayName("비정상 데이터에 대해 POST /push-message 요청이 동작하는지 테스트")
     void test2() throws Exception {
         mockMvc.perform(post("/push-message").contentType("application/json")
-                                             .content(JsonRequestDummy.NotCorrectValue))
+                                             .content(PushMessageJsonRequestDummy.EMPTY_TITLE_VALUE))
                .andExpect(status().isBadRequest());
     }
 }
---
+++ MSG-lab-api/src/testFixtures/java/com/example/msglab/PushMessageJsonResponseDummy.java
@@ -0,0 +1,13 @@
+package com.example.msglab;
+
+public class PushMessageJsonResponseDummy {
+
+    public static final String CONVERSION_EXCEPTION =
+            "{" +
+
+            "\"title\":\"your request is not convertible\"," +
+
+            "\"detail\":\"json parsing error Please check the format again\"" +
+
+            "}";
+}
