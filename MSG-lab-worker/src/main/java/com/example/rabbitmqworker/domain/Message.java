2fdd9e039eb97e6445f0f66181f37f5f95215b93
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-15 21:02:10 +0900

    Fix: Message에 주석 추가

--- MSG-lab-worker/src/main/java/com/example/rabbitmqworker/domain/Message.java
+++ MSG-lab-worker/src/main/java/com/example/rabbitmqworker/domain/Message.java
@@ -1,5 +1,6 @@
 package com.example.rabbitmqworker.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -7,6 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 푸시 메세지입니다.
+ * 전송할 내용과 전송할 주제를 담고 있습니다.
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -16,6 +21,8 @@ public class Message {
     @Id
     private String id;
 
+    // to는 MySQL 예약어입니다. to 대신 다른 단어를 사용해야합니다.
+    @Column(name = "topic")
     private String to;
 
     private Notification notification;
