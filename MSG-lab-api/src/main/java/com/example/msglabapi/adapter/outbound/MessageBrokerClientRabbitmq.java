0cfe3c0ebc20f2e450f182e5e45a2a8f0504a855
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-20 13:19:17 +0900

    common 모듈의 RabbitMQ 설정을 api, worker 모듈에서 사용하게 수정
    배경
    #83 이슈를 해결합니다.
    
    작업 내용
    삭제(api, worker 모듈의 RabbitProperty): 삭제했습니다.
    수정(common 모듈의 build.gradle): 불필요한 설정 삭제, jar 패키징 활성화
    수정(api, worker 모듈의 build.gradle): common 모듈 의존성 추가
    수정(MessageBrokerClientRabbitmq): common 모듈의 RabbitProperty import 하도록 변경
    수정(PushMessageListener): common 모듈의 RabbitProperty import 하도록 변경
    
    결과
    api, worker 모듈 각각에서 중복으로 설정하지 않습니다.
    설정 파일이 하나만 존재하게 됩니다.

--- MSG-lab-api/src/main/java/com/example/msglabapi/adapter/outbound/MessageBrokerClientRabbitmq.java
+++ MSG-lab-api/src/main/java/com/example/msglabapi/adapter/outbound/MessageBrokerClientRabbitmq.java
@@ -3,7 +3,7 @@ package com.example.msglabapi.adapter.outbound;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import com.example.msglabapi.adapter.config.RabbitProperty;
+import com.example.common.config.RabbitProperty;
 import com.example.msglabapi.application.outbound.MessageBrokerClient;
 import com.example.msglabapi.domain.Message;
 
