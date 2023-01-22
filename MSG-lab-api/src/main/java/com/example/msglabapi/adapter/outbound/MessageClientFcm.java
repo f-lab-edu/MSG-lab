377f1cff94fd7ff5834303b00cb41c414f055834
Bue-von-hon <dkssudvn2@gmail.com>
2023-01-22 23:28:32 +0900

    Resttemplate과 재요청 로직을 분리, 재요청 로직 테스트 추가
    배경
    #61 이슈를 해결하기 위한 첫번째 스텝입니다.
    강경합된 부분을 분리했습니다.
    있을법한 2가지 상황에 대해 테스트하는 코드를 추가했습니다.
    
    작업 내용
    삭제(RetryConfig): 불필요한 클래스 삭제
    추가(HttpServerErrorExceptionDetail): 익셉션 상황에 대한 디테일 클래스 추가(아직 사용 X)
    수정(MessageClientFcm): RestTemplate 대신 RestTemplate Builder를 사용하게 변경, Retryable, recover 어노테이션 추가, RestBuilder에서 타임아웃시간 설정
    수정(MsgLabApiApplication): 재요청 어노테이션 추가
    수정(MessageClientFcmTest): 실패-실패-성공 상황과 실패-실패-실패 상황 테스트 코드 추가, 각각 상황을 테스트하기 위한 Bean 추가
    
    결과
    RestTemplate과 재요청 로직이 익명 클래스 초기화 기술로 강하게 결합된 부분을 삭제함
    어노테이션을 이용하여 재요청 로직을 구현
    2가지 재요청 상황에 대한 테스트를 추가

--- MSG-lab-api/src/main/java/com/example/msglabapi/adapter/outbound/MessageClientFcm.java
+++ MSG-lab-api/src/main/java/com/example/msglabapi/adapter/outbound/MessageClientFcm.java
@@ -1,11 +1,20 @@
 package com.example.msglabapi.adapter.outbound;
 
+import java.time.Duration;
+
 import javax.annotation.PostConstruct;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.msglabapi.adapter.config.PropertyFcm;
@@ -15,42 +24,53 @@ import com.google.common.annotations.VisibleForTesting;
 
 import lombok.RequiredArgsConstructor;
 
-/**
- * FCM으로 push message 발송하는 구현체
- */
 @Component
 @RequiredArgsConstructor
 public class MessageClientFcm implements MessageClient {
 
-    private final PropertyFcm configFCM;
+    private final PropertyFcm propertyFcm;
 
     private final HttpHeaders headers = new HttpHeaders();
 
-    private final RestTemplate restTemplate;
+    private final RestTemplateBuilder restTemplateBuilder;
 
     @PostConstruct
     @VisibleForTesting
     final void init() {
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", configFCM.getAuth());
+        headers.add("Authorization", propertyFcm.getAuth());
     }
 
     @Override
-    public void send(Message message) {
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000), value = HttpServerErrorException.class)
+    public void send(final Message message) {
         final MessageRequestFcmV1 messageRequestFCM = MessageRequestFcmV1.from(message);
         final RequestEntity<MessageRequestFcmV1> request = getRequest(messageRequestFCM);
-        post(request);
+        try {
+            post(request);
+        } catch (RestClientException e) {
+            throw new HttpServerErrorException(HttpStatus.REQUEST_TIMEOUT);
+        }
     }
 
     private RequestEntity<MessageRequestFcmV1> getRequest(
             MessageRequestFcmV1 messageRequestFCM) {
         return RequestEntity
-                .post(configFCM.getUrl())
+                .post(propertyFcm.getUrl())
                 .headers(headers)
                 .body(messageRequestFCM);
     }
 
-    private void post(RequestEntity<MessageRequestFcmV1> request) {
+    private void post(final RequestEntity<MessageRequestFcmV1> request) {
+        final RestTemplate restTemplate = restTemplateBuilder
+                .setReadTimeout(Duration.ofSeconds(3000))
+                .setConnectTimeout(Duration.ofSeconds(3000))
+                .build();
         restTemplate.exchange(request, String.class);
+    }
+
+    @Recover
+    public void recoverPost(final HttpServerErrorException e) {
+        // todo(hun): 현재 retryable 메소드의 리턴값이 void입니다. 적당한 리턴 클래스를 추가해야 합니다.
     }
 }
--- MSG-lab-api/src/test/java/com/example/msglabapi/adapter/outbound/MessageClientFcmTest.java
+++ MSG-lab-api/src/test/java/com/example/msglabapi/adapter/outbound/MessageClientFcmTest.java
@@ -5,26 +5,123 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.msglab.MessageDummy;
 import com.example.msglab.PropertyFcmDummy;
+import com.example.msglabapi.adapter.config.PropertyFcm;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableRetry(proxyTargetClass = true)
 class MessageClientFcmTest {
+
+    private static final RestTemplate REST_TEMPLATE_SUCCESS = mock(RestTemplate.class);
+
+    private static final RestTemplate REST_TEMPLATE_FAIL = mock(RestTemplate.class);
+
+    private final MessageClientFcm successMessageClientFcm;
+
+    private final MessageClientFcm failMessageClientFcm;
+
+    @Autowired
+    private MessageClientFcmTest(@Qualifier("retryThenSuccess") MessageClientFcm successMessageClientFcm,
+                                 @Qualifier("retryThenFail") MessageClientFcm failMessageClientFcm) {
+        this.successMessageClientFcm = successMessageClientFcm;
+        this.failMessageClientFcm = failMessageClientFcm;
+    }
+
+    ;
 
     @Test
     @DisplayName("정상적인 입력이 들어왔을때, MessageClientFCM의 send 메소드가 호출되면 WebClient의 postForEntity가 호출되는지 테스트")
     void test1() {
+        final RestTemplateBuilder restTemplateBuilder = mock(RestTemplateBuilder.class);
         final RestTemplate restTemplate = mock(RestTemplate.class);
-
         final MessageClientFcm messageClientFCM = new MessageClientFcm(PropertyFcmDummy.CORRECT_PROPERTY,
-                                                                       restTemplate);
+                                                                       restTemplateBuilder);
+
+        when(restTemplateBuilder.build()).thenReturn(restTemplate);
+        when(restTemplateBuilder.setReadTimeout(any())).thenReturn(restTemplateBuilder);
+        when(restTemplateBuilder.setConnectTimeout(any())).thenReturn(restTemplateBuilder);
         messageClientFCM.init();
 
         messageClientFCM.send(MessageDummy.message);
-        verify(restTemplate, times(1)).exchange(any(), eq(String.class));
+        verify(restTemplate, times(1)).exchange(any(RequestEntity.class), eq(String.class));
+    }
+
+    @Test
+    @DisplayName("FCM 호출이 실패하면 재요청하는지 테스트")
+    void test2() {
+        successMessageClientFcm.send(MessageDummy.message);
+        verify(REST_TEMPLATE_SUCCESS, times(3)).exchange(any(), eq(String.class));
+    }
+
+    @Test
+    @DisplayName("FCM 호출이 실패하고, 3번 재요청도 실패하면 응답이 올바르게 내려오는지 테스트")
+    void test3() {
+        failMessageClientFcm.send(MessageDummy.message);
+        verify(REST_TEMPLATE_FAIL, times(3)).exchange(any(), eq(String.class));
+        // todo(hun): send() 메소드에 리턴값이 추가되면 테스트 코드를 추가해야함.
+    }
+
+    @Configuration
+    @EnableRetry
+    public static class SpringConfig {
+
+        @Bean
+        @Qualifier("retryThenSuccess")
+        public MessageClientFcm messageClientFcmSuccess() throws Exception {
+            final PropertyFcm propertyFcm = mock(PropertyFcm.class);
+            final RestTemplateBuilder restTemplateBuilder = mock(RestTemplateBuilder.class);
+            final ResponseEntity<String> response = new ResponseEntity<>("body", HttpStatus.ACCEPTED);
+
+            when(restTemplateBuilder.build()).thenReturn(REST_TEMPLATE_SUCCESS);
+            when(restTemplateBuilder.setReadTimeout(any())).thenReturn(restTemplateBuilder);
+            when(restTemplateBuilder.setConnectTimeout(any())).thenReturn(restTemplateBuilder);
+            when(propertyFcm.getUrl()).thenReturn("url");
+            when(REST_TEMPLATE_SUCCESS.exchange(any(), eq(String.class)))
+                    .thenThrow(new HttpServerErrorException(HttpStatus.REQUEST_TIMEOUT))
+                    .thenThrow(new HttpServerErrorException(HttpStatus.REQUEST_TIMEOUT))
+                    .thenReturn(response);
+
+            return new MessageClientFcm(propertyFcm, restTemplateBuilder);
+        }
+
+        @Bean
+        @Qualifier("retryThenFail")
+        public MessageClientFcm messageClientFcmFail() throws Exception {
+            final PropertyFcm propertyFcm = mock(PropertyFcm.class);
+            final RestTemplateBuilder restTemplateBuilder = mock(RestTemplateBuilder.class);
+
+            when(restTemplateBuilder.build()).thenReturn(REST_TEMPLATE_FAIL);
+            when(restTemplateBuilder.setReadTimeout(any())).thenReturn(restTemplateBuilder);
+            when(restTemplateBuilder.setConnectTimeout(any())).thenReturn(restTemplateBuilder);
+            when(propertyFcm.getUrl()).thenReturn("url");
+            when(REST_TEMPLATE_FAIL.exchange(any(), eq(String.class)))
+                    .thenThrow(new RestClientException("asd"));
+
+            return new MessageClientFcm(propertyFcm, restTemplateBuilder);
+        }
     }
 }
